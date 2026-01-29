a234e8ff07429e46ea675d1f4ce94a5ee828c724
Tair <tald110407@gmail.com>
2026-01-30 00:50:31 +0500

    Newest Commit

---
+++ Practice01/05Output.py
@@ -0,0 +1,16 @@
+#Printing Text Examples
+
+#1 Using multiple prints
+print("Hello, my name is Tair")
+print("I live in Almaty")
+print("I like visiting museums")
+
+#2 Quotes
+print("This is double quote string")
+print('This is single qoute string')
+print("""
+    And this is Tripple Quote
+    Multy Line String
+""")
+
+#3 
