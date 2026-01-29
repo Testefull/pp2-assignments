c54aa45a51ba3d900a5deaf7dfce7df0a925faf8
Tair <tald110407@gmail.com>
2026-01-30 00:16:38 +0500

    3. Python Syntax

---
+++ Practice01/03Syntax.py
@@ -0,0 +1,19 @@
+#Python uses indentation for a blocks of code
+if 5 > 2:
+    print("Five is greater than two!")
+
+#Syntax Error
+#if 5 > 2:
+#print("Five is greater than two!")
+
+#Programmer can choose any number of spaces for the block
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+#Programmer has to use the same number of spaces for the same block
+#Syntax Error
+#if 5 > 2:
+# print("Five is greater than two!")
+#        print("Five is greater than two!")
