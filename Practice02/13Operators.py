7cae51996261a5a0467d85d4609ecf70e440256e
Tair <tald110407@gmail.com>
2026-02-02 09:47:59 +0500

    Newest Commit

---
+++ Practice02/13Operators.py
@@ -0,0 +1,14 @@
+#ARIPHMETIC OPERATORS EXAMPLES
+
+#1 Summing, Multiplication, Division, Substraction
+a = 2 * 2
+b = 3 + (2 - 97 * 3)
+c = a / b
+
+print(f"a = {a}, b = {b}, c = {c}")
+
+#2 Division by remainder
+x = 4 % 2
+y = 1234567890
+print(f"x = {x} - remainder from devison 4 by 2")
+print(y % 10)
